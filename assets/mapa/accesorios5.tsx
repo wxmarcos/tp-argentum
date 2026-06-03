b30f7f481ef45b3f8b554c3f45ad6e8801c7158d
leandropesa <lpesa@fi.uba.ar>
2026-06-03 04:21:11 -0300

    agrego mapa bosque y mazmorra

---
+++ assets/mapa/accesorios5.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="accesorios5" tilewidth="32" tileheight="32" tilecount="1024" columns="32">
+ <image source="../imagenes/mapa/accesorios/accesorios5.png" width="1024" height="1024"/>
+</tileset>
---
+++ assets/mapa/pisos2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.0" name="pisos2" tilewidth="32" tileheight="32" tilecount="256" columns="16">
+ <image source="../imagenes/mapa/estructuras/pisos2.png" width="512" height="512"/>
+</tileset>
