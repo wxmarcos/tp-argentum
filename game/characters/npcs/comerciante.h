5cb7981edf313aacb72fd229c45acfec3e1762de
Pedro Miguel <pmiguel@fi.uba.ar>
2026-06-22 21:48:15 +0000

    refactor: eliminar clases NPC sin uso (logica ya vive en Game)

--- game/characters/npcs/comerciante.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "characters/npcs/npc.h"
-
-class Comerciante: public NPC {
-public:
-    Comerciante(int posX, int posY);
-    void interactuar(Jugador& jugador) override;  // TODO: compra/venta de items
-};
